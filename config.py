9e68b1fc5a15043216eb1370e983581281a027f9
Bobo <Bobo@Bobo-MBA.local>
2020-06-09 13:11:01 +0800

    Feat: config add index2class

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ class_map = {
         'bus': '6', 'car': '7', 'cat': '8', 'chair': '9', 'cow': '10', 'diningtable': '11', 
         'dog': '12', 'horse': '13', 'motorbike': '14', 'person': '15', 'pottedplant': '16',
         'sheep': '17', 'sofa': '18', 'train': '19', 'tvmonitor': '20'}
+index2class = {int(index): class_name for class_name, index in class_map.items()}
