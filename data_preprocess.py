d55599ecca7383f5a07771cae2bf315b47076d0d
Bobo <Bobo@Bobo-MBA.local>
2020-06-09 13:00:33 +0800

    Feat: add config file

--- data_preprocess.py
+++ data_preprocess.py
@@ -2,6 +2,7 @@
 import os
 import re
 import glob
+import config
 
 def get_bboxes(classes, xmin_list, xmax_list, ymin_list, ymax_list):
     output = []
@@ -11,11 +12,7 @@ def get_bboxes(classes, xmin_list, xmax_list, ymin_list, ymax_list):
     return output
 
 def get_voc_classidx(classes):
-    class_map = {
-            'aeroplane': '1', 'bicycle': '2', 'bird': '3', 'boat': '4', 'bottle': '5', 
-            'bus': '6', 'car': '7', 'cat': '8', 'chair': '9', 'cow': '10', 'diningtable': '11', 
-            'dog': '12', 'horse': '13', 'motorbike': '14', 'person': '15', 'pottedplant': '16',
-            'sheep': '17', 'sofa': '18', 'train': '19', 'tvmonitor': '20'}
+    class_map = config.class_map
 
     for i in range(len(classes)):
         classes[i] = class_map[classes[i]]
